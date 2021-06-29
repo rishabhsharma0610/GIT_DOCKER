6bd428f8a28185eb2f8fe727001670319e207ba4
Rishabh Sharma <R@cavisson.com>
2021-06-30 00:45:34 +0530

    hpd|cavisson

---
+++ cavisson/default/default/scripts/hpd/.version/1.0/init_script.java
@@ -0,0 +1,19 @@
+/*-----------------------------------------------------------------------------
+    Name: init_script
+    Recorded By: cavisson
+    Date of recording: 06/30/2021 12:31:28
+    Flow details:
+    Build details: 4.6.1 (build# 312)
+    Modification History:
+-----------------------------------------------------------------------------*/
+
+package hpd;
+import pacJnvmApi.NSApi;
+
+public class init_script
+{
+    public static int execute(NSApi nsApi)
+    {
+        return 0;
+    }
+}
